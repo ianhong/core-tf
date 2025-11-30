5258409c772fcf6e4b8aba60a361521c2a4e20d7
Ian Hong <ianyoungjunehong@gmail.com>
2025-11-30 14:43:29 +0000

    1. main check in

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "bucket_url" {
+  description = "The base URL of the bucket, in the format gs://<bucket-name>"
+  value       = google_storage_bucket.this.url
+}
+
+output "bucket_name" {
+  description = "The name of the bucket."
+  value       = google_storage_bucket.this.name
+}
